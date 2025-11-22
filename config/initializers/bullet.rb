0355f41158064ae7f61762c2e769d49715cc2abd
Matthew Gizzi <matthewgizzi@verizon.net>
2025-11-21 22:11:40 -0600

    Add rubocop

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+if Rails.env.local?
+  Rails.application.config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 # Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
@@ -68,11 +70,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.before(:each) do
+  config.before do
     Bullet.start_request
   end
 
-  config.after(:each) do
+  config.after do
     Bullet.perform_out_of_channel_notifications if Bullet.notification?
     Bullet.end_request
   end
