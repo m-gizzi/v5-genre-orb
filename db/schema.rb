4c96be169c1ab7097cd9bf2925ca56e949a574d5
Matthew Gizzi <matthewgizzi@verizon.net>
2025-11-23 20:43:54 -0600

    Add User and Playlist models

---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2025_11_23_031452) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  create_table "playlists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name", null: false
+    t.jsonb "raw_data", default: {}, null: false
+    t.string "spotify_id", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["spotify_id"], name: "index_playlists_on_spotify_id", unique: true
+    t.index ["user_id"], name: "index_playlists_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "access_token"
+    t.datetime "created_at", null: false
+    t.text "refresh_token"
+    t.string "spotify_display_name"
+    t.string "spotify_email"
+    t.string "spotify_id", null: false
+    t.datetime "token_expires_at"
+    t.datetime "updated_at", null: false
+    t.index ["spotify_id"], name: "index_users_on_spotify_id", unique: true
+  end
+
+  add_foreign_key "playlists", "users"
+end
