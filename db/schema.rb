c088e31e8f8997cf05cb3579e5f9dd7bb72370a5
Matthew Gizzi <matthewgizzi@verizon.net>
2025-12-23 16:59:06 -0600

    Add archived_at to playlists

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_24_035102) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_23_223533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "playlists", force: :cascade do |t|
+    t.datetime "archived_at"
     t.datetime "created_at", null: false
     t.text "description"
     t.string "name", null: false
