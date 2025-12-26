2afd82a8ff59dbc4dfeaade2a39eb648f60b3e04
Matthew Gizzi <matthewgizzi@verizon.net>
2025-12-25 23:19:48 -0600

    Cleanup Spotify::PlaylistClient

--- app/clients/spotify/playlist_client.rb
+++ app/clients/spotify/playlist_client.rb
@@ -3,30 +3,14 @@
 module Spotify
   class PlaylistClient < BaseClient
     def fetch_user_playlists(limit: 50, offset: 0)
-      handle_spotify_errors('spotify:playlists') do
+      handle_spotify_errors('spotify:users:playlists') do
         playlists = rspotify_user.playlists(limit: limit, offset: offset)
         playlists.map { |playlist| ResponseAdapters::PlaylistAdapter.adapt(playlist) }
       end
     end
 
-    def fetch_all_user_playlists
-      all_playlists = []
-      offset = 0
-      limit = 50
-
-      loop do
-        batch = fetch_user_playlists(limit: limit, offset: offset)
-        break if batch.empty?
-
-        all_playlists.concat(batch)
-        offset += limit
-      end
-
-      all_playlists
-    end
-
     def create_playlist(name:, description: nil, public: true)
-      handle_spotify_errors('spotify:playlists') do
+      handle_spotify_errors('spotify:users:create_playlist') do
         playlist = rspotify_user.create_playlist!(name, public: public, description: description)
         ResponseAdapters::PlaylistAdapter.adapt(playlist)
       end
